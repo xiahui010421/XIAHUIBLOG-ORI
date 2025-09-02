0230d08bf4f5abd6e2416357f45732c4cf9e6b6c
xiahuibb <xiahuibb@163.com>
2025-09-03 00:15:36 +0800

    自动部署: 2025-09-03 00:15:36

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # 进入 Hexo 项目目录
-cd "$(dirname "$0")"
+# cd "$(dirname "$0")"
 
 # 生成静态文件
-hexo clean
-hexo generate
+# hexo clean
+# hexo generate
 
 # 进入生成的 public 文件夹
-cd public
+# cd public
 
 # 初始化 git（如果第一次使用 public 文件夹）
-git init
+# git init
 
 # 设置远程仓库
 git remote add origin git@github.com:xiahui010421/xiahui010421.github.io.git 2> /dev/null
